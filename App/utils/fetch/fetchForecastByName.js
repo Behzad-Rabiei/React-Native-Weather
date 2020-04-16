3b6ad3e175f63c732d21f2c2720c28fc7581247c
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-17 01:32:16 +0430

    Rename fetch func and file name

--- App/utils/fetch/fetchForecastByName.js
+++ App/utils/fetch/fetchForecastByName.js
@@ -1,5 +1,5 @@
 const appID = 'ef2a0bcde3c82af19d4413af11d4ab4e';
-export const fetchForecast = (name) => {
+export const fetchForecastByName = (name) => {
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${appID}&units=imperial`,
   )
---
+++ App/utils/fetch/fetchWeatherByName.js
@@ -0,0 +1,11 @@
+const appID = 'ef2a0bcde3c82af19d4413af11d4ab4e';
+export const fetchWeatherByName = (name) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${appID}&units=imperial`,
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => console.log(error));
+};
