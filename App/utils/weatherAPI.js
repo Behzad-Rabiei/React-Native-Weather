d0a9c05726455e05cfb33c17643c66a245c9550b
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-17 12:24:58 +0430

    Refactor fetch files and handleall of them in weatherAPI.js

---
+++ App/utils/weatherAPI.js
@@ -0,0 +1,13 @@
+const appID = 'ef2a0bcde3c82af19d4413af11d4ab4e';
+
+export const weatherAPI = (path, {position, name}) => {
+  let suffix = '';
+  if (position) {
+    suffix = `lat=${position.latitude}&lon=${position.longitude}`;
+  } else if (name) {
+    suffix = `q=${name}`;
+  }
+  return fetch(
+    `https://api.openweathermap.org/data/2.5${path}?appid=${appID}&units=imperial&${suffix}`,
+  ).then((response) => response.json());
+};
