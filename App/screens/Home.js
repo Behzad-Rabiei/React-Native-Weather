f9fdaf933db258517e3ee753e2e43ce3002bb18a
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-17 12:25:20 +0430

    test weatherAPI.js

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,7 +1,43 @@
 import React, {Component} from 'react';
+import GetLocation from 'react-native-get-location';
 import {Container} from '../components/Container';
+import {weatherAPI} from '../utils/weatherAPI';
 
 class Home extends Component {
+  componentDidMount() {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+    })
+      .then((location) => {
+        this.getCurrentWeather('/weather', {
+          position: location,
+        });
+        this.getCurrentWeather('/forecast', {
+          position: location,
+        });
+        this.getCurrentWeather('/weather', {
+          name: 'Isfahan',
+        });
+        this.getCurrentWeather('/forecast', {
+          name: 'Isfahan',
+        });
+      })
+      .catch((error) => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  }
+
+  getCurrentWeather = (path, {position, name}) => {
+    weatherAPI(path, {position, name})
+      .then((response) => console.log(response))
+      .catch((error) => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  };
+
   render() {
     return <Container />;
   }
