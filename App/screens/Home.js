ffd648cdf055f3810820c7d0992255e1e53e7457
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-19 13:38:26 +0430

    StatusBar added to Home screen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,45 +1,50 @@
 import React, {Component} from 'react';
-import GetLocation from 'react-native-get-location';
+import {StatusBar} from 'react-native';
+// import GetLocation from 'react-native-get-location';
+// import {weatherAPI} from '../utils/weatherAPI';
 import {Container} from '../components/Container';
-import {weatherAPI} from '../utils/weatherAPI';
 
 class Home extends Component {
-  componentDidMount() {
-    GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 15000,
-    })
-      .then((location) => {
-        this.getCurrentWeather('/weather', {
-          position: location,
-        });
-        this.getCurrentWeather('/forecast', {
-          position: location,
-        });
-        this.getCurrentWeather('/weather', {
-          name: 'Isfahan',
-        });
-        this.getCurrentWeather('/forecast', {
-          name: 'Isfahan',
-        });
-      })
-      .catch((error) => {
-        const {code, message} = error;
-        console.warn(code, message);
-      });
-  }
+  // componentDidMount() {
+  //   GetLocation.getCurrentPosition({
+  //     enableHighAccuracy: true,
+  //     timeout: 15000,
+  //   })
+  //     .then((location) => {
+  //       this.getCurrentWeather('/weather', {
+  //         position: location,
+  //       });
+  //       this.getCurrentWeather('/forecast', {
+  //         position: location,
+  //       });
+  //       this.getCurrentWeather('/weather', {
+  //         name: 'Isfahan',
+  //       });
+  //       this.getCurrentWeather('/forecast', {
+  //         name: 'Isfahan',
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       const {code, message} = error;
+  //       console.warn(code, message);
+  //     });
+  // }
 
-  getCurrentWeather = (path, {position, name}) => {
-    weatherAPI(path, {position, name})
-      .then((response) => console.log(response))
-      .catch((error) => {
-        const {code, message} = error;
-        console.warn(code, message);
-      });
-  };
+  // getCurrentWeather = (path, {position, name}) => {
+  //   weatherAPI(path, {position, name})
+  //     .then((response) => console.log(response))
+  //     .catch((error) => {
+  //       const {code, message} = error;
+  //       console.warn(code, message);
+  //     });
+  // };
 
   render() {
-    return <Container />;
+    return (
+      <Container>
+        <StatusBar barStyle="light-content" />
+      </Container>
+    );
   }
 }
 
