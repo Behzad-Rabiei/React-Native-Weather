d3ea7ee16b4c833a926383a8741af053a82772ff
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-19 13:13:04 +0430

    Home screen headeRight added

--- App/config/routes.js
+++ App/config/routes.js
@@ -5,12 +5,29 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Home from '../screens/Home';
 import Search from '../screens/Search';
 
+import {HeaderRightButton} from '../components/HeaderRightButton';
+
+const searchIcon = require('../../assets/icons8-search-in-cloud-100.png');
+
 const MainStack = createStackNavigator();
 
 const Navigator = () => (
   <NavigationContainer>
     <MainStack.Navigator>
-      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="Home"
+        component={Home}
+        options={({navigation}) => ({
+          headerRight: () => (
+            <HeaderRightButton
+              onPress={() => {
+                navigation.navigate('Search');
+              }}
+              icon={searchIcon}
+            />
+          ),
+        })}
+      />
       <MainStack.Screen name="Search" component={Search} />
     </MainStack.Navigator>
   </NavigationContainer>
