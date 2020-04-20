ed68e49e71cebcd45e4476aa1ae05cb412296e80
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-20 22:56:01 +0430

    List component created

---
+++ App/components/List/styles.js
@@ -0,0 +1,8 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  basicRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
