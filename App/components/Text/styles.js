2a34697febedbc6d0ad922a716c5ff76644dffd0
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-21 00:20:33 +0430

    styling Text component

--- App/components/Text/styles.js
+++ App/components/Text/styles.js
@@ -3,11 +3,16 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 export default EStyleSheet.create({
   h1: {
     fontSize: 40,
+    textAlign: 'center',
+    color: '$white',
+    fontWeight: '700',
   },
   h2: {
     fontSize: 20,
+    color: '$white',
   },
   p: {
     fontSize: 16,
+    color: '$white',
   },
 });
