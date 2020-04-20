e201b51a77c0c6a2f2ddd633084d46afed62efc3
Behzad-Rabiei <behzad.rabiei.77@gmail.com>
2020-04-20 17:36:12 +0430

    Icon.js created and change other utils name

--- App/utils/weatherAPI.js
+++ App/utils/weatherAPI.js
@@ -1,3 +1,5 @@
+import {setWeather, setForecast} from '../actions/index';
+
 const appID = 'ef2a0bcde3c82af19d4413af11d4ab4e';
 
 export const weatherAPI = (path, {position, name}) => {
@@ -10,4 +12,19 @@ export const weatherAPI = (path, {position, name}) => {
   return fetch(
     `https://api.openweathermap.org/data/2.5${path}?appid=${appID}&units=imperial&${suffix}`,
   ).then((response) => response.json());
+};
+
+export const getCurrentWeather = (path, {position, name}, dispatch) => {
+  weatherAPI(path, {position, name})
+    .then((response) => {
+      if (path === '/weather') {
+        dispatch(setWeather(response));
+      } else if (path === '/forecast') {
+        dispatch(setForecast(response));
+      }
+    })
+    .catch((error) => {
+      const {code, message} = error;
+      console.warn(code, message);
+    });
 };
